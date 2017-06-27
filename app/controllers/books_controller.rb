572e09016de9a26380e887a5f45e84fe5a564497
Lia Forgione <liaforg@gmail.com>
2017-06-27 13:55:55 -0400

    step 5 with json

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,15 +7,21 @@ def index
     format.html
     format.text
 
+    format.json do
+      render json: @books.map { |book|
+      {  
+        title: book.title,
+        author: book.author,
+        already_read: book.already_read
+        }
+      }
+    end
+
     format.csv do
       render plain: @books.generate_csv(@books)
-
-        }
-      end
     end
 
 
+    end
   end
-end
-
 end
